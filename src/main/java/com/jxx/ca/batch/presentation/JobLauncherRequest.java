fb037cd71302275b4da1a82fd458a9bf7d176852
JxxHxxx <leesin5498@naver.com>
2024-01-26 17:19:24 +0900

    FEAT JobParameter id 필수 값 체크 로직 추가

--- src/main/java/com/jxx/ca/batch/presentation/JobLauncherRequest.java
+++ src/main/java/com/jxx/ca/batch/presentation/JobLauncherRequest.java
@@ -23,11 +23,11 @@ public class JobLauncherRequest {
 
         properties.putAll(this.jobParameters);
 
-        String id = properties.get("id").toString();
-
-        if (id.isEmpty() || id.isBlank()) {
-            throw new IllegalArgumentException("id 잡을 식별하는 필수 값입니다.");
+        Object idProperty = properties.get("id");
+        if (idProperty == null) {
+            throw new IllegalArgumentException("JobParameter id는 잡 식별을 위한 필수 값입니다.");
         }
+        String id = idProperty.toString();
 
         return new JobParametersBuilder(properties).addString("id", id, true)
                 .toJobParameters();
